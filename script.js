98f2830ccac9608b53d2458c59bcc0fa76a7fce6
aayushpokhrel49 <aayushpokhrel49@outlook.com>
2025-06-23 15:56:24 +0545

    Update script.js

--- script.js
+++ script.js
@@ -82,3 +82,15 @@ const animateOnScroll = function() {
 
 window.addEventListener('scroll', animateOnScroll);
 window.addEventListener('load', animateOnScroll);
+
+//newsletter 
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbw8V3iLtgvEA8tCbIwevrx3TVECXNiNwRYHGwuNKWydokYuETqfgMA8687Pa-1ytVlGzA/exec'
+  const form = document.forms['newsletter-form']
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => console.log('Success!', response))
+      .catch(error => console.error('Error!', error.message))
+  })
